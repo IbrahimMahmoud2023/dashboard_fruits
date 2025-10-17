fb900fb91bc51eef083d285d92599bcd6532828c
ibrahim mahmoud <embabyibrahim95@gmail.com>
2025-10-18 00:20:09 +0300

    configure project on firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'core/helper_function/on_generate_routes.dart';
 import 'features/dashboard_view/presentation/views/dash_board_view.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const DashBoardFruits());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(DashBoardFruits());
 }
 
 
