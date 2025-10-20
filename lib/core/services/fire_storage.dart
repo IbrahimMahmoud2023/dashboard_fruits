5b9b5ba503237bce81294bf9c0c08803faeaff0e
ibrahim mahmoud <embabyibrahim95@gmail.com>
2025-10-20 20:21:44 +0300

    finish method upload image

---
+++ lib/core/services/fire_storage.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:fruits_hub_dashboard/core/services/storage_services.dart';
+import 'package:path/path.dart' as b;
+
+class FireStorage implements StorageServices
+{
+
+  final storageRef = FirebaseStorage.instance.ref();
+  @override
+  Future<String> uploadImage(File file, String path)async {
+ String fileName =b.basename(file.path);
+ String fileExtensions = b.extension(file.path);
+ var fileReference =  storageRef.child('$path/$fileName.$fileExtensions');
+   await fileReference.putFile(file);
+   String fileDownloadUrl = await fileReference.getDownloadURL();
+   return fileDownloadUrl;
+
+
+  }
+
+
+}
---
+++ lib/core/services/storage_services.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+
+abstract class StorageServices {
+
+  Future<String> uploadImage (File file , String path) ;
+ }
