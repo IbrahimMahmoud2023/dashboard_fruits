b06dd1c94d24d510b615f2b1845236c98a33d97a
ibrahim mahmoud <embabyibrahim95@gmail.com>
2025-10-21 21:53:00 +0300

    add subabase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub_dashboard/core/services/git_it_services.dart';
 import 'package:fruits_hub_dashboard/simple_block_observer.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/helper_function/on_generate_routes.dart';
 import 'features/dashboard_view/presentation/views/dash_board_view.dart';
@@ -11,6 +12,10 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Supabase.initialize(
+    url: 'https://rvqrzdgspuvitpzogehe.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2cXJ6ZGdzcHV2aXRwem9nZWhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNjQ5MTksImV4cCI6MjA3NjY0MDkxOX0.Z7GGqEKY8Jrya83ogcukbOMav7k06chvdLz95ZWfX6Y',
+  );
   setupGetIt();
   Bloc.observer = SimpleBlocObserver();
   runApp(DashBoardFruits());
